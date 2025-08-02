c2c350cd312e193332bcd51abb8d90a7af973686
dekofar-dev <dekofardeveloper@gmail.com>
2025-08-02 11:49:19 +0300

    Add user management with role assignment and avatar upload

--- Dekofar.HyperConnect.Domain/Entities/ApplicationUser.cs
+++ Dekofar.HyperConnect.Domain/Entities/ApplicationUser.cs
@@ -8,5 +8,10 @@ namespace Dekofar.Domain.Entities
         public string? FullName { get; set; }
         //public string? Role { get; set; }
 
+        /// <summary>
+        ///     URL or file path of the user's profile image.
+        /// </summary>
+        public string? AvatarUrl { get; set; }
+
     }
 }
